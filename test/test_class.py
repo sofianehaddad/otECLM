90ecef444e50ea2aaeb22abb365701bda10b1194
Anne Dutfoy <anne.dutfoy@m4x.org>
2022-11-06 08:53:25 +0100

    Fixed test (6)

--- test/test_class.py
+++ test/test_class.py
@@ -6,8 +6,8 @@ import numpy.testing as npt
 import openturns.testing as ott
 
 def test_class():
-    values = ot.Poisson().getSample(100)
-    value = ot.Indices([int(v[0]) for v in values])
+    values = ot.Multinomial(10000, ot.Poisson().getProbabilities()).getRealization()
+    value = ot.Indices([int(v) for v in values])
     obj = otECLM.ECLM(value, ot.GaussLegendre(50))
     res = obj.estimateMaxLikelihoodFromMankamo([0.5]*4, False, False)
     ott.assert_almost_equal(res[0:4], res[0:4])
