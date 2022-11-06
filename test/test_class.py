aef5541efaafee572c73eda3eece81d4f1d25bbe
Anne Dutfoy <anne.dutfoy@m4x.org>
2022-11-06 08:30:50 +0100

    Fixed test (2)

--- test/test_class.py
+++ test/test_class.py
@@ -5,15 +5,9 @@ import pytest
 import numpy.testing as npt
 import openturns.testing as ott
 
-@pytest.fixture(scope="session")
-def data():
-    """Provide some data"""
+def test_class():
     values = ot.Poisson().getSample(1000)
-    return [int(v) for v in values]
-
-
-def test_class(data):
-    value = data
+    value = [int(v) for v in values]
     obj = otECLM.ECLM(value, ot.GaussLegendre(50))
     res = obj.estimateMaxLikelihoodFromMankamo([0.5]*4, False, False)
     ott.assert_almost_equal(res[0:4], res[0:4])
